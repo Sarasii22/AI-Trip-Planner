19368405185c3658b94c12a9612a0e30aba95257
Sarasi <eg245387@engug.ruh.ac.lk>
2026-08-25 22:34:24 +0530

    fix an error in main

--- trip_planner/main.py
+++ trip_planner/main.py
@@ -1,24 +1,34 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 from agent.agentic_workflow import GraphBuilder
-from fastapi.responses import JSONResponse
+from utils.save_to_document import save_document
+from starlette.responses import JSONResponse
 import os
-
+import datetime
+from dotenv import load_dotenv
+from pydantic import BaseModel
+load_dotenv()
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # set specific origins in prod
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 class QueryRequest(BaseModel):
-    query: str
+    question: str
 
-@app.post("/query/")
-async def query_travel_agent(query: QueryRequest):
+@app.post("/query")
+async def query_travel_agent(query:QueryRequest):
     try:
         print(query)
         graph = GraphBuilder(model_provider="groq")
         react_app=graph()
         #react_app = graph.build_graph()
 
-        #save the graph
         png_graph = react_app.get_graph().draw_mermaid_png()
         with open("my_graph.png", "wb") as f:
             f.write(png_graph)
